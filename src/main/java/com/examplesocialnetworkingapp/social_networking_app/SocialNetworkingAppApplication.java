49587b561955038a8c71b184ee5f20f99fa01987
Mayara <mayaradepaula1997>
2024-05-27 16:36:31 -0300

    first commit

---
+++ src/main/java/com/examplesocialnetworkingapp/social_networking_app/SocialNetworkingAppApplication.java
@@ -0,0 +1,13 @@
+package com.examplesocialnetworkingapp.social_networking_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examplesocialnetworkingapp/social_networking_app/SocialNetworkingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplesocialnetworkingapp.social_networking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
